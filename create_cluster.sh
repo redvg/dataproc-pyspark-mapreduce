f357119e852dbd22e740043e43099fda7493c696
redvg <redvg17@gmail.com>
2018-08-09 10:03:53 +0200

    fixes cluster script

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+   echo "Usage:  ./create_cluster.sh bucket-name folder-name"
+   exit
+fi
+
+BUCKET=$1
+FOLDER=$2
+echo "creating dataproc computing cluster"
+
 gcloud dataproc clusters create my-cluster --zone us-central1-a \
 	--master-machine-type n1-standard-1 --master-boot-disk-size 50 \
 	--num-workers 2 --worker-machine-type n1-standard-1 \
