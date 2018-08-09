e2dca59cb3ea58fdf2f5ff83e15a51f7f0d6ebfe
redvg <redvg17@gmail.com>
2018-08-09 09:20:55 +0200

    adds cluster creation script

---
+++ create_cluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gcloud dataproc clusters create my-cluster --zone us-central1-a \
+	--master-machine-type n1-standard-1 --master-boot-disk-size 50 \
+	--num-workers 2 --worker-machine-type n1-standard-1 \
+	--worker-boot-disk-size 50 --network=default \
+  --initialization-actions=gs://$BUCKET/$FOLDER/init-script.sh,gs://dataproc-initialization-actions/datalab/datalab.sh
