a86336aad7264664067f834b4f33cb0284061167
redvg <redvg17@gmail.com>
2018-08-09 09:26:02 +0200

    adds verbosity to pyspark job

--- job.py
+++ job.py
@@ -3,6 +3,8 @@
 from pyspark import SparkContext
 sc = SparkContext("local")
 
+print 'hello from pyspark---'
+
 file = sc.textFile("gs://BUCKET_NAME/FOLDER_NAME/input.txt")
 dataLines = file.map(lambda s: s.split(",")).map(lambda x : (x[0], [x[1]]))
 print dataLines.take(100)
@@ -12,3 +14,5 @@ print databyKey.take(100)
 
 countByKey = databyKey.map(lambda (k,v): (k, len(v)))
 print countByKey.take(100)
+
+print '---terminated successfully'
